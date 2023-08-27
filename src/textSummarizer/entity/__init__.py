d3974613078fa87af80046e2a8eef2996239f43d
Mayank Bansal <mayban94@gmail.com>
2023-08-27 15:34:29 +0530

    stage02

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -13,3 +13,10 @@ class DataIngestionConfig:      # this is not a normal class its dataclass
 
 # we are making our code modular so that's why we are doing it
 # instead of notebook, notebook was just experimentation
+
+# data validation entity
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -11,6 +11,7 @@ class DataIngestionTrainingPipeline:
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()  # if error regarding zipfile comes, simply comment it out and download the data manually in artifects folder
+        #data_ingestion.download_file()  # if error regarding zipfile comes, simply comment it out and download the data manually in artifects folder
         
+        # during our testing it was causing an issue, thus we are going to manually download and put the data in artifacts folder with name data.zip 
         data_ingestion.extract_zip_file()
